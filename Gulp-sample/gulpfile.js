73a80f4fc37e2b1dd29f171d78eec0ba784c8905
chenglong zhang <chenglongzhang931@gmail.com>
2016-09-19 19:03:29 -0400

    Minify the CSS file using the gulp-clean-css plugin

--- Gulp-sample/gulpfile.js
+++ Gulp-sample/gulpfile.js
@@ -1,11 +1,19 @@
 var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
-	rename = require('gulp-rename');
+	rename = require('gulp-rename'),
+	cleanCSS = require('gulp-clean-css');
 
 gulp.task('scripts', function() {
 	gulp.src('js/**/*.js')
 		.pipe(uglify())
 		.pipe(rename('app.min.js'))
 		.pipe(gulp.dest('js/'));
-
 });
+
+gulp.task('styles', function() {
+	gulp.src('css/**/*.css')
+		.pipe(cleanCSS())
+		.pipe(rename('stylesmin.css'))
+		.pipe(gulp.dest('css/'));
+});
+
