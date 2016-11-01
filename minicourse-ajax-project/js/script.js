f5fa50200a87322db719c509ff30f2d0142da9ee
chenglong zhang <chenglongzhang931@gmail.com>
2016-11-01 14:36:44 -0400

    add wikipedia links to the page

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -24,6 +24,7 @@ function loadData() {
     //load newyorktimes api
     var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' +location+ '&sort=newest&api-key=8ea7da785c9147f6836c5ca751c2f769';
     $nytHeaderElem.text('New York Times Articles About' + location);
+
     $.getJSON( url, function( data ) {
     
     var items = [];
@@ -40,6 +41,24 @@ function loadData() {
 
     }).error(function(e) {
         $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+    });
+    
+    $.ajax({
+        url: 'http://en.wikipedia.org/w/api.php?action=opensearch&search='+ cityValue +'&format=json&callback=success',
+        type: 'GET',
+        dataType: 'jsonp',
+        success: function( data ) {
+                   $wikiElem.text("Wikipedia articles about" + cityValue);
+                   for (var i = 0 , l = data[1].length; i < l; i++ ) {
+                        $wikiElem.append("<li><a href='"+ data[3][i]+"'>"+ data[1][i]+"</a></li>");
+                   }
+                }
+    }).done(function() {
+        console.log("success");
+    }).fail(function() {
+        console.log("error");
+    }).always(function() {
+        console.log("complete");
     });
     
     return false;
